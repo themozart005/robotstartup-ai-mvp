f0801ae0ffc4a493d0bc2df07337bba48229b729
themozart005 <tennyson005@gmail.com>
2025-07-15 12:07:44 -0700

    change logger import

--- frontend/src/pages/GameBoard.tsx
+++ frontend/src/pages/GameBoard.tsx
@@ -4,6 +4,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
+import logger from '../utils/logger';
 import { 
   DollarSign, 
   Cog, 
